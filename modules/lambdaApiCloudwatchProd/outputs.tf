a4ae809edda61d69ff934c516750cbd3fbe05e61
M-Farhaan-Iqbal <44829074+M-Farhaan-Iqbal@users.noreply.github.com>
2023-08-11 23:12:34 +0400

    type in res name

--- modules/lambdaApiCloudwatchProd/outputs.tf
+++ modules/lambdaApiCloudwatchProd/outputs.tf
@@ -1,4 +1,4 @@
 output "cloudwatch_log_group_arn" {
-  value       = aws_cloudwatch_log_group.lambda_log_group_prodp.arn
+  value       = aws_cloudwatch_log_group.lambda_log_group_prod.arn
   description = "ARN of the CloudWatch Log Group for Lambda function"
 }
